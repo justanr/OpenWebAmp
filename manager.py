d8991dac7319292b6a4de5c8d692869176d44bb9
justanr <alecreiter@gmail.com>
2014-09-15 22:36:04 -0400

    Added support for Flask-Migrate

--- manager.py
+++ manager.py
@@ -1,9 +1,10 @@
-from collections import namedtuple
-
-from app import app, db, ma, models, serializers, utils
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
+from app import app, db, ma, models, serializers, utils
+
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 @manager.option('-d', '--dir', dest='dir')
 def add(dir):
@@ -19,6 +20,8 @@ def _shell_context():
         serials=serializers, 
         utils=utils
         )
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
